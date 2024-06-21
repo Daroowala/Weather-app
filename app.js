ac52896b617c0e1fd8a331b850a37fe1b1ed2a6d
my name <blueshaperd3@gmail.com>
2024-06-22 01:05:43 +0500

    60% done

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputBox = document.getElementsByClassName("input-box");
+const searchBtn = document.getElementById("searchBtn");
+const weather_image = document.getElementsByClassName("weather-image");
+const temperature = document.getElementsByClassName("temperature");
+const discription = document.getElementsByClassName("discription");
+const humidity = document.getElementById("humidity");
+const wind_speed = document.getElementById("wind-speed");
