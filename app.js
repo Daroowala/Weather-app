4bdc410002002e33ad5b1ab5755dc2bdb219fa31
my name <blueshaperd3@gmail.com>
2024-06-26 00:52:23 +0500

    90% done

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const inputBox = document.getElementsByClassName("input-box");
+const inputBox = document.querySelector(".input-box");
 const searchBtn = document.getElementById("searchBtn");
-const weather_image = document.getElementsByClassName("weather-image");
-const temperature = document.getElementsByClassName("temperature");
-const discription = document.getElementsByClassName("discription");
+const weather_image = document.querySelector(".weather-image");
+const temperature = document.querySelector(".temperature");
+const discription = document.querySelector(".discription");
 const humidity = document.getElementById("humidity");
 const wind_speed = document.getElementById("wind-speed");
 
 async function checkWeather(city){
-    const apiKey = '18593d4828b0ea842043e21126c16d53';
+    const api_key = "18593d4828b0ea842043e21126c16d53";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-    const weather_date = await fetch(`${url}`).then(response=>response.json());
 
-    temperature.innerHTML = `${weather_data.main.temp}`;
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+    temperature.innerHTML = `${Math.round (weather_data.main.temp - 273.15)}C°`;
 }
 searchBtn.addEventListener('click',()=>{
     checkWeather(inputBox.value);
+   
 });
