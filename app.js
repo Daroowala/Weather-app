f6d4f3227d889e99b3bea86cb9fa941eb80f65a9
my name <blueshaperd3@gmail.com>
2024-06-25 23:49:59 +0500

    not done trying

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const discription = document.getElementsByClassName("discription");
 const humidity = document.getElementById("humidity");
 const wind_speed = document.getElementById("wind-speed");
 
-function checkWeather(city){
+async function checkWeather(city){
     const apiKey = '18593d4828b0ea842043e21126c16d53';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    const weather_date = await fetch(`${url}`).then(response=>response.json());
+
+    temperature.innerHTML = `${weather_data.main.temp}`;
 }
 searchBtn.addEventListener('click',()=>{
     checkWeather(inputBox.value);
